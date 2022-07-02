f9979c72f4085c53193fe1f90f65498cbde0e5a9
Omega172 <tjayden2002@gmail.com>
2022-07-02 00:03:59 -0500

    Refactoring and doing things right!

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 #include "il2cpp.hpp"
 #include "main.hpp"
+#include "Console.h"
 
 MethodInfo* setTime;
-
-bool bSpeed;
-
-const float speedMultiplier = 3.f;
 
 void SetTimeScale(float f) {
 	void* args[1] = { &f };
@@ -14,8 +11,12 @@ void SetTimeScale(float f) {
 	il2cpp_runtime_invoke(setTime, nullptr, args, &exc);
 }
 
-void keyHandler() {
-	for (;;) {
+void KeyHandlerLoop() {
+	bool bExit = false, bSpeed = false;
+	const float speedMultiplier = 3.f;
+	
+	do
+	{
 		/* 
 		 * If you want to use Unity's native input handling for this then feel free, 
 		 * but I don't see the point unless you're planning on using this on a game with a real anti-cheat.
@@ -24,36 +25,57 @@ void keyHandler() {
 		 * 
 		 * I've included an enum for KeyCode inside main.hpp
 		 */ 
-		if (GetAsyncKeyState(VK_F1) & 1) {
+		
+		if (GetAsyncKeyState(VK_F1) & 0x1) {
 			bSpeed = !bSpeed;
 
-			float timeScale = bSpeed ? speedMultiplier : 1.f;
+			float timeScale = bSpeed ? speedMultiplier : 1.f; // Short hand if statements are cool ~ Omega172
 			SetTimeScale(timeScale);
 		}
+		
+		if (GetAsyncKeyState(VK_F3) & 0x1)
+		{
+			bSpeed = false;
+			bExit = true;
+		}
 
-		Sleep(1);
-	}
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	} while (!bExit);
 }
 
-void main() {
+void Init()
+{
 	IL2CPP::Init();
 
-	Sleep(1500);
+	std::this_thread::sleep_for(std::chrono::milliseconds(1500));
 
 	Il2CppDomain* domain = il2cpp_domain_get();
 
 	if (!domain) {
-		cout << "domain = null\n" << endl;
+		std::cout << "domain = null\n" << std::endl;
 		return;
 	}
 
 	il2cpp_thread_attach(domain);
 
-	cout << "IL2CPP Initialised." << endl;
+	std::cout << "IL2CPP Initialised." << std::endl;
 
 	Il2CppImage* unityCore = il2cpp_domain_assembly_open(domain, "UnityEngine.CoreModule")->image;
 	Il2CppClass* timeClass = il2cpp_class_from_name(unityCore, "UnityEngine", "Time");
 	setTime = (MethodInfo*)il2cpp_class_get_method_from_name(timeClass, "set_timeScale", 1);
+}
 
-	keyHandler();
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	Console con = Console::instance(true);
+	con.setTitle("Universal IL2CPP Speed");
+	std::cout << "Injected!" << std::endl;
+	
+	Init();
+
+	KeyHandlerLoop();
+
+	con.free();
+	FreeLibraryAndExitThread((HMODULE)lpReserved, EXIT_SUCCESS); // Unload DLL ~ Omega172
+	return 0;
 }
